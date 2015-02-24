d59e2b71b27905ed4a8fb52ac69c41b70140af58
pakorn <pakorn@the-c-level.com>
2015-02-24 20:42:29 +0700

    update

---
+++ src/main/java/com/cms/model/db/FileUploadModel.java
@@ -0,0 +1,50 @@
+package com.cms.model.db;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.hibernate.annotations.Proxy;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "fileupload")
+@Proxy(lazy=false)
+public class FileUploadModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="topic")
+    private String topic;
+
+    @Column(name="location")
+    private String location;
+
+    @OneToOne
+    @JoinColumn(name="subject_id")
+    private SubjectModel subjectModel;
+
+    @OneToOne
+    @JoinColumn(name="class_id")
+    private ClassModel classModel;
+
+    @OneToOne
+    @JoinColumn(name="assignment_id")
+    private AssignmentModel assignmentModel;
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .append("id", id)
+                .append("topic", topic)
+                .append("location", location)
+                .append("subjectModel", subjectModel)
+                .append("classModel", classModel)
+                .append("assignmentModel", assignmentModel)
+                .toString();
+    }
+}
---
+++ src/main/java/com/cms/model/db/StudentAssignmentModel.java
@@ -0,0 +1,50 @@
+package com.cms.model.db;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.hibernate.annotations.Proxy;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "student_assignment")
+@Proxy(lazy=false)
+public class StudentAssignmentModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @OneToOne
+    @JoinColumn(name="student_id")
+    private UserModel userModel;
+
+    @OneToOne
+    @JoinColumn(name="assignment_id")
+    private AssignmentModel assignmentModel;
+
+    @Column(name="submit_status")
+    private boolean submitStatus;
+
+    @Column(name="score")
+    private BigDecimal score;
+
+    @Column(name="upload_assignment")
+    private String uploadAssignment;
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .append("id", id)
+                .append("userModel", userModel)
+                .append("assignmentModel", assignmentModel)
+                .append("submitStatus", submitStatus)
+                .append("score", score)
+                .append("uploadAssignment", uploadAssignment)
+                .toString();
+    }
+}
