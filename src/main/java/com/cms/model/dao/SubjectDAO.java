189ade719596d066aff810b6ba7ef7f5882e60ab
pakorn <pakorn@the-c-level.com>
2015-02-27 22:14:26 +0700

    update

--- src/main/java/com/cms/model/dao/SubjectDAO.java
+++ src/main/java/com/cms/model/dao/SubjectDAO.java
@@ -1,8 +1,29 @@
 package com.cms.model.dao;
 
 import com.cms.model.db.SubjectModel;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository
 public class SubjectDAO extends GenericDAO<SubjectModel, Integer> {
+
+    public List<SubjectModel> findByUserId(int userId){
+        List<SubjectModel> subjectModelList = new ArrayList<SubjectModel>();
+
+        try {
+            Criteria criteria = getCriteria();
+            criteria.add(Restrictions.eq("userModel.id", userId));
+            criteria.addOrder(Order.asc("subjectId"));
+            subjectModelList = criteria.list();
+        } catch (Exception e) {
+            log.debug("Exception error findByUserId : ", e);
+        }
+
+        return subjectModelList;
+    }
 }
