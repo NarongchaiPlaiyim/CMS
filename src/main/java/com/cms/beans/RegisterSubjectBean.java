1358bc975c7552235e41cb70bb458b024bd90428
Chai <narongchai@the-c-level.com>
2015-03-02 20:17:42 +0700

    menu

--- src/main/java/com/cms/beans/RegisterSubjectBean.java
+++ src/main/java/com/cms/beans/RegisterSubjectBean.java
@@ -58,7 +58,7 @@ public class RegisterSubjectBean extends Bean{
 
     private void subjectInTeacher(){
         subjectModelList =  registerSubjectService.findByTeacherId(teacherId);
-        httpSession.setAttribute("studentSubject", subjectModelList);
+        httpSession.setAttribute(AttributeName.STUDENT_SUBJECT.getName(), subjectModelList);
         log.debug("--------- {}", subjectModelList.size());
     }
 
